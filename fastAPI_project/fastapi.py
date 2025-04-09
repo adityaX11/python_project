8734a70e44b826819ce6c5bfd967ff70904c4699
Aditya kumar <adicpr1234@gmail.com>
2025-04-10 02:14:30 +0530

    create a new repo for python projects. and here we have create a project which based on fastAPI along with unicorn and pydantic.

---
+++ fastAPI_project/fastapi.py
@@ -0,0 +1,51 @@
+"""
+What is fastAPI->
+              FastAPI is a modern web framework for building APIs with Python — and it’s FAST as hell 🚀.
+            It’s built on top of Starlette and Pydantic
+
+           💡 Why is FastAPI lit?=>>>
+                Super fast performance (almost like Node.js or Go)
+
+                Auto generates docs for your API (Swagger UI 😍)     
+
+                Works great with async/await 🔁
+
+                Validates your data with Pydantic (no need to write tons of if-else)
+
+                Used by big dogs like Netflix, Microsoft, Uber 🐺   
+
+            🤖 Use Cases:=>>>
+                Building AI/ML APIs for models
+
+                Backend for mobile/web apps
+
+                Real-time dashboards
+
+                Chatbots, automation tools
+
+  >>>>> FastAPI generally work with unicorn (ASGI) .
+     what is unicorn->>        
+     Uvicorn is a lightning-fast ASGI web server for Python apps.
+     It's like the engine that runs your FastAPI app — kinda like how Node.js runs Express apps.       
+
+  >>>>> One of this that is know as Pydantic  
+  what is pydantic  ->>
+    Pydantic is a Python library that helps you:
+
+    ✅ Validate data
+    ✅ Enforce data types
+    ✅ Convert data automatically
+
+    Basically, it’s your data babysitter 👶
+    It makes sure everything going into your app is clean, correct, and the right type.
+        
+     ->>How does FastAPI use Pydantic?
+        FastAPI is BUILT around Pydantic — it uses it for:
+
+        Request validation (POST, PUT, etc.)
+
+        Response formatting
+
+        Automatic docs generation
+        
+"""
